a39751e8e36d16d8d076ed65c741b84b4a188526
iiidjediii <iiidjediii@mail.ru>
2020-10-27 16:55:05 +0300

    first commit

---
+++ catalog/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = [('first_name', 'last_name'), ('date_of_birth', 'date_of_death')]
+admin.site.register(Author, AuthorAdmin)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+#admin.site.register(Book)
+#admin.site.register(Author)
+
+#admin.site.register(BookInstance)
+
+
+admin.site.register(Genre)
