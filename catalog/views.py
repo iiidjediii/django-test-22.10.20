6f13544ad13f08a0fd6606a77ead17c70d26b215
iiidjediii <iiidjediii@mail.ru>
2020-11-02 17:27:55 +0300

    comment:'new functions'

--- catalog/views.py
+++ catalog/views.py
@@ -4,6 +4,9 @@ from catalog.models import Book, Author, BookInstance, Genre
 
 from django.views import generic
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 def index(request):
     """View function for home page of site."""
 
@@ -16,12 +19,16 @@ def index(request):
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits, # add for sessions
     }
 
     # Render the HTML template index.html with the data in the context variable
@@ -29,3 +36,26 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 5
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 5
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
